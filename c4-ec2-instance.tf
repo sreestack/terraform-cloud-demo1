3d668ebd04f07884c470cd452df62f5bf9faeb15
sreekanthkurapati <sreekanthkurapati@gmail.com>
2022-05-14 20:40:37 +0530

    second changes

--- c4-ec2-instance.tf
+++ c4-ec2-instance.tf
@@ -2,8 +2,8 @@
 resource "aws_instance" "my-ec2-vm" {
   ami           = data.aws_ami.amzlinux.id 
   instance_type = var.instance_type
-  count = 3
-  key_name      = "terraform-key"
+  count = 1
+  key_name      = "terraform-key1"
   user_data = file("apache-install.sh")  
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
